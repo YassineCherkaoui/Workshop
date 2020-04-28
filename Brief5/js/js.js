07f7db112963265a886e2af7b3334596966142d6
YassineCherkaoui <57252454+YassineCherkaoui@users.noreply.github.com>
2020-04-28 21:57:47 +0000

    Project finich

--- Brief5/js/js.js
+++ Brief5/js/js.js
@@ -1,4 +1,4 @@
-//validation java script
+
 function formValidation() {
   var uid = document.registration.userid;
   var passid = document.registration.passid;
@@ -9,43 +9,6 @@ function formValidation() {
   var uemail = document.registration.email;
   var umsex = document.registration.msex;
   var ufsex = document.registration.fsex;
-  var regex = /^[a-zA-Z0-9.-]+@[a-z0-9.-]{2,6}\.[a-z]{2,4}$/;
-  var regexpasswd = /^[a-zA-Z*'-_^"@$#]{8,30}$/;
-  var regexname = /^[a-zA-Z]{3,16}$/;
-  var cpt = 0;
-  if (uid == "" || passid == "" || uname == "" || uadd == "" || uemail == "") {
-    window.alert(" Tous les champs doivent être remplis");
-    cpt++;
-  }
-
-  else {
-    if (!regex.test(uemail) || !regexpasswd.test(passid) || !regexname.test(uname) || !regexname.test(ucountry)) {
-      window.alert(" Respectez les formats des elements entrés ");
-      cpt++;
-    }
-  }
-
-  if (cpt == 0) {
-    alert("validé !");
-  }
-
-}
-
-
-
-
-
-
-// function formValidation() {
-//   var uid = document.registration.userid;
-//   var passid = document.registration.passid;
-//   var uname = document.registration.username;
-//   var uadd = document.registration.address;
-//   var ucountry = document.registration.country;
-//   var uzip = document.registration.zip;
-//   var uemail = document.registration.email;
-//   var umsex = document.registration.msex;
-//   var ufsex = document.registration.fsex;
   
 //     if (userid_validation(uid, 5, 12)) {
 //       if (passid_validation(passid, 7, 12)) {
@@ -65,85 +28,85 @@ function formValidation() {
 //     return false;
 
 //   }
-// //   if (userid_validation(uid, 5, 12) || passid_validation(passid, 7, 12) || passid_validation(passid, 7, 12) || passid_validation(passid, 7, 12) 
-// //     || allLetter(uname) || alphanumeric(uadd) || countryselect(ucountry) || allnumeric(uzip) || ValidateEmail(uemail) || validsex(umsex, ufsex)){
+  if (userid_validation(uid, 5, 12) && passid_validation(passid, 7, 12) && passid_validation(passid, 7, 12) && passid_validation(passid, 7, 12) 
+    && allLetter(uname) && alphanumeric(uadd) && countryselect(ucountry) && allnumeric(uzip) && ValidateEmail(uemail) || validsex(umsex, ufsex)){
  
-// // return false;
-// // }
-// // };
+return false;
+}
+};
 
-// function userid_validation(uid, min, mx) {
-//   var uid_len = uid.value.length;
-//   if (uid_len == 0 || uid_len < min && uid_len > mx) {
-//     alert("User Id should not be empty / length be between " + min + " to " + mx);
-//     uid.focus();
-//     return false;
-//   }
-//   return true;
-// }
+function userid_validation(uid, min, mx) {
+  var uid_len = uid.value.length;
+  if (uid_len == 0 || uid_len < min && uid_len > mx) {
+    alert("User Id should not be empty / length be between " + min + " to " + mx);
+    uid.focus();
+    return false;
+  }
+  return true;
+}
 
-// function passid_validation(passid, min, mx) {
-//   var passid_len = passid.value.length;
-//   if (passid_len == 0 || passid_len < min && passid_len > mx) {
-//     alert("Password should not be empty / length be between " + min + " to " + mx);
-//     passid.focus();
-//     return false;
-//   }
-//   return true;
-// }
+function passid_validation(passid, min, mx) {
+  var passid_len = passid.value.length;
+  if (passid_len == 0 || passid_len < min && passid_len > mx) {
+    alert("Password should not be empty / length be between " + min + " to " + mx);
+    passid.focus();
+    return false;
+  }
+  return true;
+}
 
-// function allLetter(uname) {
-//   var letters = /^[A-Za-z]+$/;
-//   if (uname.value.match(letters)) {
-//     return true;
-//   } else {
-//     alert('Username must have alphabet characters only');
-//     uname.focus();
-//     return false;
-//   }
-// }
+function allLetter(uname) {
+  var letters = /^[A-Za-z]+$/;
+  if (uname.value.match(letters)) {
+    return true;
+  } else {
+    alert('Username must have alphabet characters only');
+    uname.focus();
+    return false;
+  }
+}
 
-// function alphanumeric(uadd) {
-//   var letters = /^[0-9a-zA-Z]+$/;
-//   if (uadd.value.match(letters)) {
-//     return true;
-//   } else {
-//     alert('User address must have alphanumeric characters only');
-//     uadd.focus();
-//     return false;
-//   }
-// }
+function alphanumeric(uadd) {
+  var letters = /^[0-9a-zA-Z]+$/;
+  if (uadd.value.match(letters)) {
+    return true;
+  } else {
+    alert('User address must have alphanumeric characters only');
+    uadd.focus();
+    return false;
+  }
+}
 
-// function countryselect(ucountry) {
-//   if (ucountry.value == "Default") {
-//     alert('Select your country from the list');
-//     ucountry.focus();
-//     return false;
-//   } else {
-//     return true;
-//   }
-// }
+function countryselect(ucountry) {
+  if (ucountry.value == "Default") {
+    alert('Select your country from the list');
+    ucountry.focus();
+    return false;
+  } else {
+    return true;
+  }
+}
 
-// function allnumeric(uzip) {
-//   var numbers = /^[0-9]+$/;
-//   if (uzip.value.match(numbers)) {
-//     return true;
-//   } else {
-//     alert('ZIP code must have numeric characters only');
-//     uzip.focus();
-//     return false;
-//   }
-// }
+function allnumeric(uzip) {
+  var numbers = /^[0-9]+$/;
+  if (uzip.value.match(numbers)) {
+    return true;
+  } else {
+    alert('ZIP code must have numeric characters only');
+    uzip.focus();
+    return false;
+  }
+}
 
-// function ValidateEmail(uemail) {
-//   var mailformat = /^[A-Za-z-0-9-_.]+@[A-Za-z].[A-Za-z]{2,3}$/;
-//   if (uemail.value.match(mailformat)) {
-//     return true;
-//   } else {
-//     alert("You have entered an invalid email address!");
-//     return false;
-//   }
-// }
+function ValidateEmail(uemail) {
+  var mailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+  if (uemail.value.match(mailformat)) {
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    return false;
+  }
+}
 
 //slider
 var slideIndex = 1;
